cb12f3de854b011fdafb5a53c1bf6141704ba0a9
Arik Md. Isthiaque <arikbncc@gmail.com>
2019-12-14 00:39:00 +0600

    Update hpp.py

--- hpp.py
+++ hpp.py
@@ -1,22 +1,29 @@
-import cv2
 import numpy as np
+import cv2
 
-im = cv2.imread('input_2.jpg', cv2.IMREAD_GRAYSCALE)
+im = cv2.imread('input_2.jpg')
+imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(imgray,127,255,0)
+im, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.drawContours(im,contours,-1,(255,0,0),2)
+
+for h,cnt in enumerate(contours):
+    mask = np.zeros(imgray.shape,np.uint8)
+    cv2.drawContours(mask,[cnt],0,255,-1)
+    mean = cv2.mean(im,mask = mask)
+
+    x, y, w, h = cv2.boundingRect(cnt)
+    print('X: ', x, 'Y: ', y, 'W: ', w, 'H: ', h)
+    cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
 
-im = 255 - im
-
-
-proj = np.sum(im,1)
-
-
-m = np.max(proj)
-w = 500
-result = np.zeros((proj.shape[0],500))
-
-
-for row in range(im.shape[0]):
-   cv2.line(result, (0,row), (int(proj[row]*w/m),row), (255,255,255), 1)
-
-
-cv2.imwrite('output_2.jpg', result)
+cv2.imshow('image', im)
+k = cv2.waitKey(0) & 0xFF
+# press ESC to close  
+if k == 27:  
+    cv2.destroyAllWindows() 
+# press s to save the output     
+elif k == ord('s'):  
+    cv2.imwrite('result_of_hpp_and_vpp.jpg',im) 
+    cv2.destroyAllWindows() 
